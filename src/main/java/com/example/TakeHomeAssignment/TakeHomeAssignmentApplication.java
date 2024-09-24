37aa188981f69394517f9cfe95d293a17d43402e
Hardik <hardik11dhiman@gmail.com>
2024-09-24 13:06:46 +0530

    Project setup

---
+++ src/main/java/com/example/TakeHomeAssignment/TakeHomeAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.TakeHomeAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeHomeAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeHomeAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TakeHomeAssignment/TakeHomeAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TakeHomeAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeHomeAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
